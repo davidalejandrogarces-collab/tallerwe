3657164d45883336be884368ed555d14a2ca0159
Juan Pantoja <nakaaka56@gmail.com>
2026-05-13 00:42:38 -0500

    agregacion de clases

---
+++ src/main/java/univalle/edu/co/fpoe/modelo/Asignatura.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package univalle.edu.co.fpoe.modelo;
+
+/**
+ *
+ * @author Sala Sistemas
+ * aqui van las entidades, dentro de este paquete
+ */
+public class Asignatura {
+    private String codigo;
+    private String nombre;
+    private byte creditos;
+    private byte intensidad;
+    
+    public Asignatura(){
+        
+    }
+
+    public Asignatura(String codigo, String nombre, byte creditos, byte intensidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.intensidad = intensidad;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public byte getCreditos() {
+        return creditos;
+    }
+
+    public byte getIntensidad() {
+        return intensidad;
+    }
+    
+    
+    
+}
